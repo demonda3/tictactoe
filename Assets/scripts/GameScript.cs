9ae2ef1527bfdfb2c7a6fa8e7d568156c99997da
demonda3 <bellakatto@gmail.com>
2024-05-30 20:28:48 -0700

    Add files via upload

---
+++ Assets/scripts/GameScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+   public class GameScript : MonoBehaviour
+{
+int spriteIndex = -1;
+
+    public int PlayerTurn()
+    {
+        spriteIndex++;
+        return spriteIndex % 2;
+    }
+
+}
+
